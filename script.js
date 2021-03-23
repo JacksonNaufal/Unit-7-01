84a0aaeec9cc11ada69a031a0d23e82ab090a092
Jackson-Naufal <jackson.naufal@ocsbstudent.ca>
2021-03-23 13:20:21 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+document.getElementById('button').addEventListener('click',button)
+
+let randomnumber = 0
+
+let userGuess = 0
+
+
+function button () {
+  userGuess = document.getElementById('input').value
+  userGuess = parseInt(userGuess)
+  // recieve the number, and convert it to an interger number.
+  randomnumber = (Math.random() * 6) + 1
+  //choose random number between 0 and 1, but add 6 + 1 so no zeros
+  // compare random numbers
+  if (userGuess >= randomnumber) {
+    document.getElementById('para').innerHTML =
+    'Good Job!'
+
+  }
+
+
+
+}
