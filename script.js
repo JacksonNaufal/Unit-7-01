2466cd4c7140f3c800d17f123bab9c01c68e607f
Jackson-Naufal <jackson.naufal@ocsbstudent.ca>
2021-03-23 13:45:00 +0000

    Minor changes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.getElementById('button').addEventListener('click',button)
+document.getElementById('button').addEventListener('click', button)
 
 let randomnumber = 0
 
@@ -9,7 +9,7 @@ function button () {
   userGuess = parseInt(userGuess)
   // recieve the number, and convert it to an interger number.
   randomnumber = (Math.random() * 6) + 1
-  //choose random number between 0 and 1, but add 6 + 1 so no zeros
+  // choose random number between 0 and 1, but add 6 + 1 so no zeros
   // compare random numbers
   if (userGuess >= randomnumber) {
     document.getElementById('para').innerHTML =
